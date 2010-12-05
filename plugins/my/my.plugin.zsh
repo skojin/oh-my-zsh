1efaac9682c73f23910d2a15ed368ad2a4e128ec
Sergey Kojin <sergey.kojin@gmail.com>
2010-12-05 19:54:33 +0300

    keep my options

---
+++ plugins/my/my.plugin.zsh
@@ -0,0 +1,9 @@
+
+#  speed up rails tests in REE http://smartic.us/2010/10/27/tune-your-ruby-enterprise-edition-garbage-collection-settings-to-run-tests-faster
+#export RUBY_HEAP_MIN_SLOTS=1000000
+#export RUBY_HEAP_SLOTS_INCREMENT=1000000
+#export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+#export RUBY_GC_MALLOC_LIMIT=1000000000
+#export RUBY_HEAP_FREE_MIN=500000
+
+alias ls='ls -lh'
